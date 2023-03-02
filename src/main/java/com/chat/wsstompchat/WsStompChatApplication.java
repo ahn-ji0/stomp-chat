8e5cbdc8b01751c38b7cb08a1634f492ec180843
Jiyoung <ahn.jiyoung15@gmail.com>
2023-03-02 13:48:07 +0900

    init commit

---
+++ src/main/java/com/chat/wsstompchat/WsStompChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.wsstompchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsStompChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsStompChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chat/wsstompchat/WsStompChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.wsstompchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsStompChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
